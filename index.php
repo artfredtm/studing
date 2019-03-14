fe60357831c271b812a963439be3a440367b6f7e
Artem.FRED <artem@fredtm.ru>
2019-03-14 11:42:32 +0400

    TestTest

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Формы</title>
+</head>
+<body>
+	<form>
+		
+		<label for="name" name="name" id="name"></label>
+		<input type="text" placeholder="Логин"><br />
+
+		<label for="email" name="email" id="email"></label><br />
+		<input type="email" placeholder="Электронная почта"><br />
+
+		<label for="password" name="password" id="password"></label><br />
+		<input type="password" placeholder="Пароль"><br />
+
+		<br /><br />
+		<script src = "./post.js"></script>
+		<input type="submit" name="submit" id="button" value="Жмяк">
+				<script src = "./post.js"></script>
+		<input type="submit" name="submit" id="button" value="Жмяк">
+	</form>
+
+
+</body>
+</html>

6b3e891c188a6445b256b0c90163c02d26aef10f
Artem.FRED <artem@fredtm.ru>
2019-03-14 09:17:28 +0400

    Изменил файл index.php

--- index.php
+++ index.php
@@ -10,36 +10,11 @@
 		<label for="name" name="name" id="name"></label>
 		<input type="text" placeholder="Логин"><br />
 
- 					@$get = $_GET['error']; 
-                    if($get == 'login'){
-                        echo "
-                        <div class='error'>
-                            <h3>Логин не верный!</h3>
-                        </div>
-                        ";
-                    }
-
 		<label for="email" name="email" id="email"></label><br />
 		<input type="email" placeholder="Электронная почта"><br />
 
-                   if($get == 'email'){
-                        echo "
-                        <div class='error'>
-                            <h3>Неверный адрес электронной почты!</h3>
-                        </div>
-                        ";
-                    }
-
 		<label for="password" name="password" id="password"></label><br />
 		<input type="password" placeholder="Пароль"><br />
-
-                   if($get == 'pass'){
-                        echo "
-                        <div class='error'>
-                            <h3>Пароль не верный!</h3>
-                        </div>
-                        ";
-                    }
 
 		<br /><br />
 		<script src = "./post.js"></script>
